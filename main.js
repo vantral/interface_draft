cdc14bb15bff3f6cc37770253aa93f0db43e7da4
vantral <anton.buzanov.00@gmail.com>
2022-11-09 14:57:14 +0300

    bag fixed

--- main.js
+++ main.js
@@ -6,16 +6,18 @@ interact('.dropzone').dropzone({
     // only accept elements matching this CSS selector
     accept: '.drag-drop',
     // Require a 75% element overlap for a drop to be possible
-    overlap: 0.75,
+    overlap: 0.5,
 
     // listen for drop related events:
 
     ondropactivate: function (event) {
         // add active dropzone feedback
+        console.log('ondropactivate')
         event.target.classList.add('drop-active')
         event.relatedTarget.classList.add('dragging')
     },
     ondragenter: function (event) {
+        console.log('ondragenter')
         var draggableElement = event.relatedTarget
         var dropzoneElement = event.target
         
@@ -26,6 +28,7 @@ interact('.dropzone').dropzone({
 
     },
     ondragleave: function (event) {
+        console.log('ondragleave')
         // remove the drop feedback style
         event.target.classList.remove('drop-target')
         event.relatedTarget.classList.remove('can-drop')
@@ -33,13 +36,11 @@ interact('.dropzone').dropzone({
     },
     ondrop: function (event) {
         // event.relatedTarget.textContent = 'Dropped'
-
-        console.log(event)
+        console.log('ondrop')
         const draggableElements = [...event.target.querySelectorAll('.drag-drop:not(.dragging)')]
         left = draggableElements.reduce((closest, child) => {
             const box = child.getBoundingClientRect()
             const offset = event._interaction.coords.cur.client.x - box.left - box.width / 3
-            // console.log(offset)
             if (offset < 0 && offset > closest.offset) {
                 return { offset: offset, element: child }
             } else {
@@ -64,20 +65,53 @@ interact('.dropzone').dropzone({
         }
     },
     ondropdeactivate: function (event) {
+        box = event.relatedTarget.getBoundingClientRect()
+        if (
+            !event.relatedTarget.parentElement.classList.contains('group') ||
+            box.bottom > event._interaction.coords.cur.client.y || box.top < event._interaction.coords.cur.client.y
+        ) {
+            event.relatedTarget.style.transform = 'translate(0px, 0px)'
+            event.relatedTarget.setAttribute('data-x', 0)
+            event.relatedTarget.setAttribute('data-y', 0)
+            if (event.relatedTarget.parentElement.classList.contains('group')) {
+                event.relatedTarget.classList.add('can-drop')
+            }
+        }
         // remove active dropzone feedback
         event.target.classList.remove('drop-active')
         event.target.classList.remove('drop-target')
         event.relatedTarget.classList.remove('dragging')
+        // document.getElementById('inner-dropzone').style.height = "fit-content"
+    }
+})
+  
+interact('.drag-drop').draggable({
+    onstart: function (event) {
+        console.log('onstart')
+        let target = event.target
+        let position = target.getBoundingClientRect()
+        target.style.position = "fixed"
+        target.style.top = position.top + "px"
+    },
+    onend: function (event) {
+        let target = event.target
+        target.style.position = "relative"
+        target.style.top = "auto"
+        
+        y = event._interaction.coords.cur.client.y
+        dropzone = document.querySelector('#inner-dropzone')
+        if (y > dropzone.getBoundingClientRect().bottom) {
+            target.classList.remove('can-drop')
+            document.getElementById('start').appendChild(target)
+        }
         document.querySelectorAll('.group').forEach(group => {
             if (group.textContent == "") {
                 group.remove()
             }
         })
-    }
-})
-  
-interact('.drag-drop').draggable({
-    inertia: true,
+        // console.log(event)
+    },
+    inertia: false,
     modifiers: [
     interact.modifiers.restrictRect({
         endOnly: true
@@ -97,7 +131,6 @@ function dragMoveListener (event) {
     
     // translate the element
     target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-    
     // update the posiion attributes
     target.setAttribute('data-x', x)
     target.setAttribute('data-y', y)
